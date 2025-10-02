0c15c6a74eb1ee8dc6bcb02f91dcafe07ed1ccab
Akekaweez <77050796+akekawee01@users.noreply.github.com>
2025-10-02 06:18:11 +0000

    Implement code changes to enhance functionality and improve performance

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -10,5 +10,15 @@ with DAG(
 
     t1 = EmptyOperator(task_id="t1")
     t2 = EmptyOperator(task_id="t2")
+    t3 = EmptyOperator(task_id="t3")
+    t4 = EmptyOperator(task_id="t4")
+    t5 = EmptyOperator(task_id="t5")
+    t6 = EmptyOperator(task_id="t6")
+    t7 = EmptyOperator(task_id="t7")
+    t8 = EmptyOperator(task_id="t8")
+    t9 = EmptyOperator(task_id="t9")
 
-    t2 >> t1
+    t1 >> t2 >> t3 >> t4 >> t9
+    t1 >> t2 >> t6 >> t8 >> t9
+    t1 >> t5 >> t7 >> t9
+    t1 >> t5 >> t6 >> t8 >> t9
