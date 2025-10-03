5dce61125a256a28887ec8cc14b7289236474393
Akekaweez <77050796+akekawee01@users.noreply.github.com>
2025-10-03 07:50:31 +0000

    g

--- dags/workshop.py
+++ dags/workshop.py
@@ -3,17 +3,34 @@ from airflow.decorators import dag, task
 from airflow.providers.standard.operators.empty import EmptyOperator
 import pandas as pd
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 @task
 def etl():
     pg_hook = PostgresHook(postgres_conn_id="my_postgres_connection")
 
     customers_df = pg_hook.get_df("SELECT * FROM customers;")
-    print(customers_df)
 
-    customers_df = pd.read_csv(customers_df)
-    # Format the datetime from "12 May 1990" to "1990-05-12"
-    df["birthdate"] = pd.to_datetime(df["birthdate"], dayfirst=True)
-    print(df["birthdate"])
+    # If you want to add a new column to the DataFrame with some result, for example, let's add a column "age"
+
+    # Calculate age from birthdate
+    customers_df["birthdate"] = customers_df["birthdate"].apply(
+        lambda x: datetime.now().year - pd.to_datetime(x, dayfirst=True).year
+    )
+    # Save DataFrame to a Parquet file
+    parquet_file = "s3://pea-watt/akeeee/2025-10-03/customers.parquet"
+    customers_df.to_parquet(parquet_file, index=False)
+
+    # Upload Parquet file to S3 using S3Hook
+    s3_hook = S3Hook(aws_conn_id="my_aws_connection")
+    s3_bucket = "pea-watt"
+    s3_key = "customers/customers.parquet"
+    s3_hook.load_file(
+        filename=parquet_file,
+        key=s3_key,
+        bucket_name=s3_bucket,
+        replace=True
+    )
 
 
 
