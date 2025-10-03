1322db01091f6da5c303d0bf66cf96ba9a363914
Akekaweez <77050796+akekawee01@users.noreply.github.com>
2025-10-03 07:32:28 +0000

    sdafd

---
+++ dags/workshop.py
@@ -0,0 +1,25 @@
+from airflow.sdk import DAG
+from airflow.utils import timezone
+from airflow.providers.standard.operators.empty import EmptyOperator
+import pandas as pd
+
+@task
+def etl():
+    pg_hook = PostgresHook(postgres_conn_id="my_postgres_connection")
+
+    customers_df = pg_hook.get_df("SELECT * FROM customers;")
+
+
+    customers_df = pd.read_csv(...)
+    # Format the datetime from "12 May 1990" to "1990-05-12"
+    df["birthdate"] = pd.to_datetime(df["birthdate"], dayfirst=True)
+    print(df["birthdate"])
+
+
+
+with DAG(
+    "workshop",
+    start_date=timezone.datetime(2025,10,2),
+    schedule=None,
+):
+    # Idea
