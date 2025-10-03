bb49bbd3bed4392e8ef9aec35d0daa4345c0be2c
Akekaweez <77050796+akekawee01@users.noreply.github.com>
2025-10-03 08:02:23 +0000

    g

--- dags/workshop.py
+++ dags/workshop.py
@@ -10,15 +10,18 @@ def etl():
     pg_hook = PostgresHook(postgres_conn_id="my_postgres_connection")
 
     customers_df = pg_hook.get_df("SELECT * FROM customers;")
+    orders_df = pg_hook.get_df("SELECT * FROM orders;")
 
     # If you want to add a new column to the DataFrame with some result, for example, let's add a column "age"
 
     customers_df["birthdate"] = pd.to_datetime(customers_df["birthdate"]).dt.strftime("%d-%m-%Y")
     print(customers_df["birthdate"])
+    print(orders_df)
     # Save DataFrame to a local Parquet file
     parquet_file = "/tmp/customers.parquet"
     customers_df.to_parquet(parquet_file, index=False)
-
+    order_parquet_file = "/tmp/orders.parquet"
+    orders_df.to_parquet(order_parquet_file, index=False)
     # Upload Parquet file to S3 using S3Hook
     s3_hook = S3Hook(aws_conn_id="my_aws_connection")
     s3_bucket = "pea-watt"
@@ -29,6 +32,17 @@ def etl():
         bucket_name=s3_bucket,
         replace=True
     )
+
+    s3_hook = S3Hook(aws_conn_id="my_aws_connection")
+    s3_bucket = "pea-watt"
+    s3_key = "akeeee/2025-10-03/orders.parquet"
+    s3_hook.load_file(
+        filename=order_parquet_file,
+        key=s3_key,
+        bucket_name=s3_bucket,
+        replace=True
+    )
+
 
 
     objects = s3_hook.list_keys(
