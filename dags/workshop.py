9337543d17873dd19fe0eeecd4315c8540d753fe
Akekaweez <77050796+akekawee01@users.noreply.github.com>
2025-10-03 07:38:54 +0000

    g

--- dags/workshop.py
+++ dags/workshop.py
@@ -8,7 +8,7 @@ def etl():
     pg_hook = PostgresHook(postgres_conn_id="my_postgres_connection")
 
     customers_df = pg_hook.get_df("SELECT * FROM customers;")
-
+    print(customers_df)
 
     customers_df = pd.read_csv(...)
     # Format the datetime from "12 May 1990" to "1990-05-12"
